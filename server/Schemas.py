3537ced5f80771f6fd342585e2a2b83c0037ea66
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-08-01 20:06:08 +0300

    implement UserSchema and NoteSchema using marshmallow_SQLAlchemy

--- server/Schemas.py
+++ server/Schemas.py
@@ -1,5 +1,5 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-from mashmallow import fields
+from marshmallow import fields
 from server.models import User,Note
 from server.extension import db
 
@@ -7,13 +7,20 @@ class UserSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = User
         load_instance = True
-        exclude = ("password_hash")
-
+        sqla_session = db.session
+        exclude = ("password_hash",)
+# allow plain password instead of serializing the hash password
     password = fields.String(load_only=True,required=True)
 
 class NoteSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Note
         load_instance = True
-        include_relationship = True
+        sqla_session =db.session
+        include_fk= True
 
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+note_schema = NoteSchema()
+notes_schema = NoteSchema( many=True)
